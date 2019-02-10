7819b4e13b1bb82f20b7b10fbca6d8598261f401
sam ghoreishi <sam20gh@gmail.com>
2019-02-10 19:39:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
-  array.join(" and ")
+
+  elsif array.length == 2
+    array.insert(1, "and").join(" ")
+  else
+    str = ", and " + array.pop
+    str = array.join(", ") << str
+  end
+end
 
 end
